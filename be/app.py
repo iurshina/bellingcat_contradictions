433703c2d1593ee8127e573f27ad941b18089429
Kristina Nedzvetskaia <kristina_nedzvetskaia@epam.com>
2023-11-16 13:26:29 +0100

    CORS

--- be/app.py
+++ be/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, BackgroundTasks
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import os
 
@@ -7,6 +8,19 @@ from contradictions_detector import detect_contradictions
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8080",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class DirectoryPath(BaseModel):
     path: str
